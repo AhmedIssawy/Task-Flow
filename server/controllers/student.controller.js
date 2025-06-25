410a17e3ebec3d41fe5319a4729914196db9dab5
0xOral <alqudsisours@gmail.com>
2025-06-25 20:45:52 +0300

    sec/mass-assignment

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -155,8 +155,11 @@ const getStudentById = asyncHandler(async (req, res) => {
 });
 
 const updateStudent = asyncHandler(async (req, res) => {
-  const { lang = "en", ...updates } = req.body;
+  const { lang = "en" } = req.body;
   const { id } = req.params;
+
+  const allowedFields = ["name", "email", "courses", "universityId"];
+  const updates = Object.fromEntries(Object.entries(req.body).filter(([k]) => allowedFields.includes(k)));
 
   const updatedStudent = await Student.findByIdAndUpdate(
     id,
