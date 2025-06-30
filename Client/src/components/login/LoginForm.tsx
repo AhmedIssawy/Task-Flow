0784c9bcdc2a5da9e21b667348f2983da4e141ef
Khaled <aa.khaled11@gmail.com>
2025-06-30 13:39:39 +0300

    add role based login

--- Client/src/components/login/LoginForm.tsx
+++ Client/src/components/login/LoginForm.tsx
@@ -11,19 +11,33 @@ import { useLanguage } from '@/hooks/useLanguage';
 import { normalizeRole } from '@/utils/role';
 import { useAppDispatch } from '@/store/hooks';
 import { setAuth } from '@/store/slices/authSlice';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 export default function LoginForm() {
   const [userId, setUserId] = useState('');
+  const [userIdRole, setUserIdRole] = useState('');
   const [password, setPassword] = useState('');
   const [login, { isLoading, isError }] = useLoginMutation();
   const router = useRouter();
   const t = useTranslations('auth.login');
   const { isRTL } = useLanguage();
   const dispatch = useAppDispatch();
+  const fullUserId = `${userIdRole}${userId}`;
+console.log('Full User ID:', fullUserId);
+console.log('User ID Role:', userIdRole);
+
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const { role, data } = await login({ id: userId, password }).unwrap();
+      const { role, data } = await login({ id: fullUserId, password }).unwrap();
+      console.log('User ID Role:', userIdRole);
       console.log('role and data', role, data);
 
       const redirectPath = getPathByRole(role, data.id);
@@ -52,14 +66,26 @@ export default function LoginForm() {
         isRTL ? 'text-right' : 'text-left'
       }`}
     >
-      <Input
-        placeholder={t('email')}
-        value={userId}
-        onChange={(e) => setUserId(e.target.value)}
-        type="text"
-        required
-        className={isRTL ? 'text-right' : 'text-left'}
-      />
+      <div className="flex">
+        <Select onValueChange={(value) => setUserIdRole(value)}>
+          <SelectTrigger className="w-[100px]">
+            <SelectValue placeholder="Role" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="STU-">Student</SelectItem>
+            <SelectItem value="ADMIN-">Admin</SelectItem>
+            <SelectItem value="TEA-">Teacher</SelectItem>
+          </SelectContent>
+        </Select>
+        <Input
+          placeholder="ID"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          type="text"
+          required
+          className={isRTL ? 'text-right' : 'text-left'}
+        />
+      </div>
       <Input
         placeholder={t('password')}
         value={password}
