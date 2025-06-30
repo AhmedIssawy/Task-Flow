bc9d05a8a1d81796fd5bb6099c9b2f2a862a84ca
Your Name <your.email@example.com>
2025-06-30 22:26:52 +0300

    added language middle ware and applied it

---
+++ server/middlewares/user prefrences/language.prefrence.js
@@ -0,0 +1,16 @@
+const detectLanguage = () => {
+  return (req, res, next) => {
+    const lang = req.cookies?.lang || req.headers["lang"];
+    if (!req.cookies?.lang){
+        res.cookie("lang", lang, {
+            httpOnly: false,
+            maxAge: 24 * 60 * 60 * 365 * 1000, // 1 year
+        })
+    }
+    req.lang = lang;
+    console.log("Detected language:", lang);
+    next();
+  };
+};
+
+export default detectLanguage;
