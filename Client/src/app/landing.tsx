7078f43aa2b48981a3a0e31140113eba3dd7cc89
Khaled <aa.khaled11@gmail.com>
2025-07-22 15:52:43 +0300

    fix landing components naming

--- Client/src/app/landing.tsx
+++ Client/src/app/landing.tsx
@@ -1,14 +1,14 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const Header = dynamic(() => import("@/components/landing/header"), { ssr: false });
+const Header = dynamic(() => import("@/components/landing/Header"), { ssr: false });
 const HeroSection = dynamic(() => import("@/components/landing/HeroSection"), { ssr: false });
-const Features = dynamic(() => import("@/components/landing/features"), { ssr: false });
-const Benefits = dynamic(() => import("@/components/landing/benefits"), { ssr: false });
+const Features = dynamic(() => import("@/components/landing/Features"), { ssr: false });
+const Benefits = dynamic(() => import("@/components/landing/Benefits"), { ssr: false });
 const Analytics = dynamic(() => import("@/components/landing/Analytics"), { ssr: false });
 const OurWork = dynamic(() => import("@/components/landing/Ourwork"), { ssr: false });
-const Testimonials = dynamic(() => import("@/components/landing/testimonials"), { ssr: false });
-const Footer = dynamic(() => import("@/components/landing/footer"), { ssr: false });
+const Testimonials = dynamic(() => import("@/components/landing/Testimonials"), { ssr: false });
+const Footer = dynamic(() => import("@/components/landing/Footer"), { ssr: false });
 
 const LandingPage = () => {
   return (
